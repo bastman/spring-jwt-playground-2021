4bce1d7abaf4a411290b278caa214eac52b8bc4e
sebastian.schmidt <bastman77@gmail.com>
2021-09-13 09:27:32 +0200

    basic-auth

--- app/src/main/kotlin/com/example/demo/rest/ApiConfig.kt
+++ app/src/main/kotlin/com/example/demo/rest/ApiConfig.kt
@@ -1,15 +1,17 @@
 package com.example.demo.rest
 
+import com.example.demo.config.security.basicauth.BasicAuthConfig
 import com.example.demo.config.security.jwt.resourceserver.JwtAuthConfig
 import com.example.demo.config.security.jwt.resourceserver.toAuthStrategyName
 import org.springframework.stereotype.Component
 
 @Component
 class ApiConfig(
-    private val jwtAuthConfig: JwtAuthConfig
+    private val jwtAuthConfig: JwtAuthConfig,
+    private val basicAuthConfig: BasicAuthConfig
 ) {
     val title: String
-        get() = "Demo App - API (auth: ${jwtAuthConfig.toAuthStrategyName()})"
+        get() = "Demo App - API (jwt-auth: ${jwtAuthConfig.toAuthStrategyName()} basic-auth.enabled: ${basicAuthConfig.enabled})"
 }
 
 
